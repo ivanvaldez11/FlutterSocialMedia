8fe92210ba400715fb450f45416dc88020827c48
Ivan <ivanvaldez140@gmail.com>
2022-07-04 02:36:57 -0400

    Comments

--- fluttermedia/lib/main.dart
+++ fluttermedia/lib/main.dart
@@ -2,28 +2,108 @@ import 'dart:async';
 
 import 'dart:html';
 
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:agora_rtc_engine/rtc_local_view.dart'; as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_remote_view.dart'; as RtcRemoteView;
 import 'package:flutter/material.dart';
-import 'package:fluttermedia/pages/Login.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+
+const appId = "b8a814db045c47c381a8234b21df6e09";
+const token = "<-- Insert Token -->";
+const channel = "test";
+
+void main() => runApp(MaterialApp(home: MyApp()));
+
+class MyApp extends StatefulWidget{
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _MyAppState extends State<MyApp>{
+  int? _remoteUid;
+  bool _localUserJoined = false;
+  late RtcEngine _engine;
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+  @override 
+  void initState(){
+    super.initState();
+    initAgora();
+  }
+
+  Future<void> initAgora() async{
+
+    await window.navigator.getUserMedia(audio: true, video: true);
+
+
+    _engine = await RtcEngine.create(appId);
+    await _engine.enableVideo();
+    _engine.setEventHandler(
+      RtcEngineEventHandler(
+        joinChannelSuccess: (String channel, int uid, int elapsed){
+          print("local user $uid joined");
+          setState(() {
+            _localUserJoined = true;
+          });
+        },
+        userJoined: (int uid, int elapsed) {
+          print("remote user $uid joined");
+          setState(() {
+            _remoteUid = uid;
+          });
+        },
+        userOffline: (int uid, UserOfflineReason reason) {
+          print("remote user $uid left channel");
+          setState(() {
+            _remoteUid = null;
+          });
+        },
       ),
-      home: Login(),
+    );
+
+    await _engine.joinChannel('', channel, null, 0);
+  }
+
+
+@override
+Widget build(BuildContext context){
+  return Scaffold(
+    appBar: AppBar(
+    title: const Text('Video Call'),
+    ),
+    body: Stack(
+      children: [
+        Center(
+          child: _remoteVideo(),
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+            width: 100,
+            height: 150,
+            child: Center(
+              child: _localUserJoined
+              ? RtcLocalView.SurfaceView()
+              : CircularProgressIndicator(),
+              ),
+            ),
+            ),
+      ],
+    ),
+  );
+}
+
+
+Widget _remoteVideo(){
+  if (_remoteUid != null){
+    return RtcRemoteView.SurfaceView(
+      uid: _remoteUid!,
+      channelId: channel,
+    );
+  } else {
+    return Text(
+      'Please wait for remote user to join',
+      textAlign: TextAlign.center,
     );
   }
+}
 }
